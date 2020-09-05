277f74bf688689ba2485f24e42144e2fc154a083
arsvlad <arsvlad2020@gmail.com>
2020-09-05 19:32:42 +0300

    Final

--- src/ru/geekbrains/javacore1/Employee.java
+++ src/ru/geekbrains/javacore1/Employee.java
@@ -2,8 +2,52 @@ package ru.geekbrains.javacore1;
 
 public class Employee {
 
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee (String name, String position, String email, String phone, int salary, int age ) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public static void intro() {
+
+        System.out.println();
+        System.out.println("СОТРУДНИКИ КОМПАНИИ");
+        System.out.println();
+
+    }
+
+    public static void intro1() {
+
+        System.out.println();
+        System.out.println("СОТРУДНИКИ КОМПАНИИ СТАРШЕ 40 ЛЕТ");
+        System.out.println();
+
+    }
 
 
+    public int getAge() {
+        return age;
+    }
 
-
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/ru/geekbrains/javacore1/Main.java
+++ src/ru/geekbrains/javacore1/Main.java
@@ -1,10 +1,48 @@
 package ru.geekbrains.javacore1;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
 
+        Employee[] emps = new Employee[5];
+
+
+        emps[0] = new Employee("Ivanov Sergey", "Manager", "is@comp.com", "30120", 100000, 45);
+        emps[1] = new Employee("Sergeev Ivan", "Engineer", "si@comp.com", "30125", 700000, 37);
+        emps[2] = new Employee("Petrov Alex", "Driver", "pa@comp.com", "30135", 60000, 28);
+        emps[3] = new Employee("Sidorov Vlad", "Courier", "sv@comp.com", "30138", 40000, 23);
+        emps[4] = new Employee("Vekshin Gleb", "Guard", "vg@comp.com", "30141", 55000, 41);
+
+
+        Employee.intro();
+
+
+        for (int i = 0; i < emps.length; i++) {
+
+            System.out.printf("%10s\n", emps[i]);
+        }
+
+
+        Employee.intro1();
+
+        for (int i = 0; i < emps.length; i++) {
+
+            if (emps[i].getAge() > 40) {
+
+                System.out.printf("%10s\n", emps[i]);
+            }
+
+
+        }
     }
+
 }
+
+
+
+
+
+
